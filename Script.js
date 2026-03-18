3aee1eef70a2c490cbaeb1c8993b942d99f92503
satyam04bpit <satyamkumar10102007@gmail.com>
2026-03-19 01:03:07 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,132 @@
+// var age= 26;
+
+
+// if (true){
+//     console.log(age);
+// }
+// let a=10;
+// // let b=20;
+// console.log(a++); // output: 10
+// console.log(a);  // output: 11
+// let a="10";
+// a+=10;
+// console.log(a)
+// let b="10";
+// console.log(a===b);e
+
+// let day=2;
+// switch(day){case 1:
+//     console.log("Monday");
+//     break;
+// case 2:
+//     console.log("Tuesday");
+//     break;
+// case 3:
+//     console.log("Dayname:Wednesday");
+//     break;
+// case 4:
+//     console.log("Thursday");
+//     break;
+// case 5:
+//     console.log("Friday");
+//     break;
+// case 6:
+//     console.log("Saturday");
+//     break;
+// case 7:
+//     console.log("Sunday");
+//     break;
+// default:
+//     console.log("Invalid day number");
+// }   
+// console.log("Counting from 1 to 20:");
+// for(let i=1;i<=20;i++){
+//     console.log(i);--
+// console.log("Reverse counting from 20 to 1:");
+
+// for(let i=20;i>=1;i--){if(i==10)
+// {continue;}
+//     console.log(i);
+// }
+// let i=1;
+// while(i<=20){if(i==11){continue;}console.log(i,"hello");
+//     i++;}
+    
+    // let name="satyam is a good boy";
+    // console.log(name.toUpperCase());
+    // console.log(name[0]);
+    // console.log(name.slice(0,3));
+    // console.log(name.trim());
+    // console.log(name.concat(" kumar"));
+    // console.log(name.split(" "));
+    // console.log(name.length);
+    // function defination
+    // function saymyName() {
+    //     console.log("Love Babbar");
+    // }
+    //to use the function we have to call the function
+   // saymyName();
+   //using loop in a fuction
+// function printCounting(){for(let i=1;i<=20;i++){console.log(i);}}
+ //calling the function
+// printCounting();
+// function printNumbers(num){
+//     console.log("printing numbers:",num);}
+// printNumbers(78);
+// function getMyName(firstName,LastName){
+//     let fullname=firstName + " " + LastName;
+//     return fullname;    
+// }
+// let value=getMyName("Satyam","Kumar");
+// console.log(value);
+// let person={
+//     name:"Satyam",
+//     age:26,
+//     city:"Bangalore",
+//     greet:function(){
+//         console.log("Hello, my name is " + this.name );
+//     }
+//    };
+// console.log(person);
+// person.greet();
+// console.log(typeof (person));
+// let fruits=["apple","banana","orange"];
+// console.log(fruits);
+// console.log(fruits.length);.
+// let numbers=[1,2,3,4,5];
+// console.log(numbers.map(function(num){return num+45;}));
+// console.log(numbers.indexOf(2));
+// console.log(numbers.filter(function(num){return num%2==0;}));
+// sayhello();
+// function sayhello(){
+//     console.log("Kaise ho saare ke saare log bhai kyu itna naraz ho rhe ho");
+// }
+
+// console.log(x);
+// const x=10;
+// class person {
+//     constructor(name,age, weight){
+//         this.name= name;
+//         this.age= age;
+//         this.weight= weight;}
+//         greet(){
+//             console.log("Hello my name is " + this.name +".I am " + this.age + " years old and my weight is " + this.weight+ " kg");
+//     }
+    
+//     //creating objects
+// }
+// let p1 =new person("satyam",10,52); 
+// let p2 =new person("Rohit",45,62);
+// p1.greet();
+// p2.greet();
+ function add(a , b ){
+   console.log(a + b);
+}
+
+add(5,3);
+add(5);
+add();
+console.log(Math.round(4.7));
+console.log(Math.floor(4.7));
+console.log(Math.ceil(4.7));
+console.log(Math.round());
